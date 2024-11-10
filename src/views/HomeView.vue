dfe23f54998946f3aa1e2555078b68f40b21cb13
CristopherBuitrago <cristopherbuitrago0907@gmail.com>
2024-11-10 17:44:29 -0500

    feat(agregado): :sparkles: Se agrego clase 3
    Clase sobre breakpoints - visibility, es decir, los elementos que se pueden ver en una anchura y altura determinada. (RESPONSIVE)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
     <h1>Home page</h1>
+    <!-- se oculta de 1265px hacia abajo -->
+    <v-card elevation="8" class="dark pink pa-5 ma-5 hidden-md-and-down">
+      <h2 class="white--text">This will show since 1265px and down</h2>
+      <v-btn class="orange">Click me</v-btn>
+    </v-card>
+
+    <!-- se oculta de 1265px hacia abajo -->
+    <v-card elevation="8" class="dark orange pa-5 ma-5 hidden-md-and-up">
+      <h2 class="white--text">This will show since 1265px and up </h2>
+      <v-btn class="pink">Don't click me</v-btn>
+    </v-card>
   </div>
 </template>
 
