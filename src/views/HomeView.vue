6030ac89cfe5523ef9b5d2e6c2bc3cb1a73a1ba5
CristopherBuitrago <cristopherbuitrago0907@gmail.com>
2024-11-10 15:28:42 -0500

    feat(agregado): :sparkles: Se agrego clase 1
    propiedades para backgrounds, font-size, text-color y mas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="home">
     <h1>Home page</h1>
+    <!-- to add background use: color -->
+    <!-- to add color text use: color--text -->
+    <h2>Background and Text color</h2>
+    <p class="black grey--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, itaque velit? Corporis eius maxime esse, nostrum itaque iusto cumque excepturi commodi harum dolore dolores distinctio nesciunt, provident rerum molestiae officiis.</p>
+    <p class="grey black--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, itaque velit? Corporis eius maxime esse, nostrum itaque iusto cumque excepturi commodi harum dolore dolores distinctio nesciunt, provident rerum molestiae officiis.</p>
+    
+    <!-- to add lighten use: lighten-number -->
+    <h2>Lighten background</h2>
+    <p class="black lighten-1 grey--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, itaque velit? Corporis eius maxime esse, nostrum itaque iusto cumque excepturi commodi harum dolore dolores distinctio nesciunt, provident rerum molestiae officiis.</p>
+    <p class="grey lighten-1 black--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, itaque velit? Corporis eius maxime esse, nostrum itaque iusto cumque excepturi commodi harum dolore dolores distinctio nesciunt, provident rerum molestiae officiis.</p>
+    
+    <!-- to add darker text use: text--dark-number -->
+    <h2>Dark text</h2>
+    <p class="black text--darken-2 grey--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, itaque velit? Corporis eius maxime esse, nostrum itaque iusto cumque excepturi commodi harum dolore dolores distinctio nesciunt, provident rerum molestiae officiis.</p>
+    <p class="grey text--darken-2 black--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, itaque velit? Corporis eius maxime esse, nostrum itaque iusto cumque excepturi commodi harum dolore dolores distinctio nesciunt, provident rerum molestiae officiis.</p>
+    
+    <h2>Font size</h2>
+    <p class="yellow display-4">Lorem</p>
+    <p class="blue display-3">Lorem</p>
+    <p class="red display-2">Lorem</p>
+    <p class="green display-1">Lorem</p>
+    
   </div>
 </template>
 
